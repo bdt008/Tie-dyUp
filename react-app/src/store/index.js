aa92bc3a8693b4badc6182663925c32eaecf9733
Bryant Del Toro <bdt008@Bryants-MacBook-Pro.local>
2023-02-27 15:47:06 -0500

    Components mapped out

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import cartReducer from './carts';
 import productReducer from './products';
+import reviewsReducer from './reviews';
 import session from './session'
 import storeReducer from './stores';
 
